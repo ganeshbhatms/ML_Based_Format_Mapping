2a479c9b9a9ed8b1b676575e42a6d82595df164b
Ganesh Bhat M S <msganeshbhat@gmail.com>
2020-08-02 21:50:30 +0530

    Update supervised.py

--- supervised.py
+++ supervised.py
@@ -63,7 +63,7 @@ def supervisedLearning():
     knn = KNeighborsClassifier(n_neighbors=1)
     clf = knn.fit(X_train_tfidf, encode)
 
-    pickle.dump(clf, open('/home/binarymonk/Desktop/ml/model/text_matching.pickle', 'wb'))
+    pickle.dump(clf, open('text_matching.pickle', 'wb'))
     
     return json.dumps({"sourceformatName": importData['source'].get('formatName'),
                "targetformatName": importData['target'].get('formatName'),

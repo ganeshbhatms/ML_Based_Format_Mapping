45df1cc3cf493fcc47f8c219e97fee4342a36108
Ganesh Bhat M S <msganeshbhat@gmail.com>
2020-08-07 21:00:46 +0530

    Update unsupervised.py

--- unsupervised.py
+++ unsupervised.py
@@ -1,67 +1,32 @@
 import pandas as pd
-import json
 import numpy as np
+import json
 from pickle5 import pickle
-
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.preprocessing import LabelEncoder
-
-import re
-import string
-
 from flask import request, Flask
+from simtext import CosineSimilarity, FitModel
 
 app = Flask(__name__)
 
+
 @app.route('/train/format/learn', methods=['POST'])
 def unsupervisedLearning():
-    def ngrams(text, n=2):
-        remove = string.punctuation
-        remove = remove.replace("#", "")
-        pattern = r"[{}]".format(remove)
-        text = re.sub(pattern,r'', text)
-        ngrams = zip(*[text[i:] for i in range(n)])
-        return [''.join(ngram) for ngram in ngrams]
-
-
-    def cosine_sim(text1, text2):
-        count_vect = CountVectorizer(analyzer=ngrams)
-        tfidf_transformer = TfidfTransformer()
-        
-        targetText = "".join([x.lower() for x in text2])
-        sourceText = "".join([y.lower() for y in text1])
-        vec1 = [sourceText, targetText]
-        vec2 = count_vect.fit_transform(vec1)
-        tfidf = tfidf_transformer.fit_transform(vec2)
-        similarity = ((tfidf * tfidf.T).A)[0,1]
-        return int(round(similarity*100))
-
+    """
+    This function is mapped to the POST request of the REST interface
+    """
     importData = request.get_json()
     trainData = pd.DataFrame(importData['mappings'])
-    targetData = importData['target']['formatFields']
-    
-    trainData['confidence']= cosine_sim(trainData['sourceField'],trainData['targetField'])
-    
-    le = LabelEncoder()
-    encode = le.fit_transform(trainData['sourceField'])
-    
-    count_vect = CountVectorizer(analyzer=ngrams)
-    X_train_counts = count_vect.fit_transform(trainData['targetField'])
-    tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-    
-    # KNN Classifier
-    from sklearn.neighbors import KNeighborsClassifier
-    knn = KNeighborsClassifier(n_neighbors=1)
-    clf = knn.fit(X_train_tfidf, encode)
 
-    pickle.dump(clf, open('/home/binarymonk/Desktop/ml/model/text_learning.pickle', 'wb'))
-    
+    trainData['confidence'] = [CosineSimilarity(x, y) for x, y in
+                               zip(trainData['sourceField'], trainData['targetField'])]
+
+    clf = FitModel(trainData['sourceField'], trainData['targetField'])
+    pickle.dump(clf, open('text_learning.pickle', 'wb'))
+
     return json.dumps({"sourceformatName": importData['source'].get('formatName'),
-               "targetformatName": importData['target'].get('formatName'),
-               "overallConfidence": np.mean(trainData['confidence']),"Message" : "Learned the mappings"})
+                       "targetformatName": importData['target'].get('formatName'),
+                       "overallConfidence": np.mean(trainData['confidence']),
+                       "Message": "Learned the mappings"})
 
-    
-if __name__ =='__main__':
-    app.run(debug=True, port=4001)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
